e21865277915b6b683e4548bf7d46992c3dc9e08
大师兄 <1275054668@qq.com>
2021-08-13 23:36:41 +0800

    更改为正式版本北极狐配置

--- composelib/src/main/java/com/ananananzhuo/composelib/constants/Constants.kt
+++ composelib/src/main/java/com/ananananzhuo/composelib/constants/Constants.kt
@@ -8,22 +8,26 @@ package com.ananananzhuo.composelib.constants
 
 //路由的跳转常量
 const val PAGE1="page1"
-const val PAGE2="page1"
-const val PAGE3="page2"
-const val PAGE4="page3"
-const val PAGE5="page4"
-const val PAGE6="page5"
-const val PAGE7="page6"
+const val PAGE2="page2"
+const val PAGE3="page3"
+const val PAGE4="page4"
+const val PAGE5="page5"
+const val PAGE6="page6"
+const val PAGE7="page7"
 const val PAGE8="page8"
 const val PAGE9="page9"
 const val PAGE10="page10"
 const val PAGE11="page11"
 const val PAGE12="page12"
 const val PAGE13="page13"
-const val PAGE14="page714"
+const val PAGE14="page14"
 const val PAGE15="page15"
 const val PAGE16="page16"
 const val PAGE17="page17"
+const val PAGE18="page18"
+const val PAGE19="page19"
+const val PAGE20="page20"
+const val PAGE21="page21"
 //路由的跳转常量
 
 
