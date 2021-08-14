51686eb4e8dd51397ccbd3c85fea531037f86a37
大师兄 <1275054668@qq.com>
2021-08-14 08:03:23 +0800

    增加主题

---
+++ composelib/src/main/java/com/ananananzhuo/composelib/Theme.kt
@@ -0,0 +1,34 @@
+package com.ananananzhuo.composelib
+
+import androidx.compose.ui.graphics.Color
+
+
+/**
+ * author  :mayong
+ * function:
+ * date    :2021/8/14
+ **/
+
+//private val DarkColorPalette = darkColors(
+//    primary = Purple200,
+//    primaryVariant = Purple700,
+//    secondary = Teal200
+//)
+//
+//private val LightColorPalette = lightColors(
+//    primary = Purple500,
+//    primaryVariant = Purple700,
+//    secondary = Teal200
+//
+//    /* Other default colors to override
+//    background = Color.White,
+//    surface = Color.White,
+//    onPrimary = Color.White,
+//    onSecondary = Color.Black,
+//    onBackground = Color.Black,
+//    onSurface = Color.Black,
+//    */
+//)
+val statusbarColor: Color = Color(0, 153, 136)
+
+val toolbarColor: Color = Color(0, 153, 136)
